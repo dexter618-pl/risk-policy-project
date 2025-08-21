2acc263c6484bf509ef8bee3f655e311a3e57bf9
Dexter Olusola <dexterolu@outlook.com>
2025-08-21 09:12:48 +0100

    Second commit - frontend ready for Render

--- src/api.js
+++ src/api.js
@@ -4,10 +4,10 @@ import axios from "axios";
 const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:8000";
 
 // Fetch all assessments
-export const fetchAssessments = async () => {
-  const res = await axios.get(`${API_BASE}/assessments`);
-  return res.data;
-};
+//export const fetchQuestions = async () => {
+  //const res = await axios.get(`${API_BASE}/questions`);
+  //return res.data;
+//};
 
 // Fetch all questions
 export const fetchQuestions = async () => {
